36080b182b5a255ed6f07b630bad999ccdde199f
thecitizenhall-cloud <thecitizenhall@gmail.com>
2026-04-20 20:06:20 -0400

    Delete pages/expert.jsx

--- pages/expert.jsx
+++
@@ -1,3 +0,0 @@
-import dynamic from "next/dynamic";
-const ExpertScreen = dynamic(() => import("../components/ExpertScreen"), { ssr: false });
-export default function Expert() { return <ExpertScreen />; }
