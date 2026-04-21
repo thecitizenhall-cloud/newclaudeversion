949e8204f5a68b253406f8aa0a100e8237476c3d
thecitizenhall-cloud <thecitizenhall@gmail.com>
2026-04-20 20:06:38 -0400

    Delete pages/notifications.jsx

--- pages/notifications.jsx
+++
@@ -1,3 +0,0 @@
-import dynamic from "next/dynamic";
-const NotificationsScreen = dynamic(() => import("../components/NotificationsScreen"), { ssr: false });
-export default function Notifications() { return <NotificationsScreen />; }
