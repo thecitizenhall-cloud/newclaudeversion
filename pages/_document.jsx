5bb30471a27ceb32bd9f170af938ce28e808465b
thecitizenhall-cloud <thecitizenhall@gmail.com>
2026-05-01 14:26:06 -0400

    Add favicon and apple touch icon links

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,9 @@ export default function Document() {
         <meta name="theme-color" content="#0F0E0C"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <link rel="icon" href="/favicon.ico" sizes="any"/>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+        <link rel="apple-touch-icon" href="/favicon-16.png"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet"/>
